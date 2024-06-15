bcef041deea638ea5e336482ba7b599ed17e13ab
carlos676767 <carlitobr730@gmail.com>
2024-06-15 12:29:33 -0700

    http put ok

--- request.js
+++ request.js
@@ -1,11 +1,11 @@
 const httpRequest = async () => {
   try {
-    const httpRequest = await fetch("http://localhost:8080/post", {
-      method: "POST",
+    const httpRequest = await fetch("http://localhost:8080/edit", {
+      method: 'PUT',
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ nome: "carlos", cpf: "444747847474666ghbgh" }),
+      body: JSON.stringify({nome: "silva", cpf: "" })
     });
     const res = await httpRequest.json()
     console.log(res);
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const  bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const api = express();
 require("dotenv").config();
 const url = `mongodb+srv://admin:${process.env.PASS_BANCO}@dados.7d94myt.mongodb.net/`;
 const client = new MongoClient(url);
-api.use(bodyParser.json())
+api.use(bodyParser.json());
 
 api.get("/get", async (data, resp) => {
   try {
@@ -16,16 +16,30 @@ api.get("/get", async (data, resp) => {
   }
 });
 
-api.post("/post",  async(data, req) => {
+api.post("/post", async (data, req) => {
   try {
-    const dados = data.body
-    const {nome, cpf} = dados
-    const dataBase = await databaseCreate(nome, cpf)
+    const dados = data.body;
+    const { nome, cpf } = dados;
+    await databaseCreate(nome, cpf);
     req.send({ resposta: "OK", status: 200 });
   } catch (error) {
     console.log(error);
   }
-})
+});
+
+api.put("/edit", async (data, req) => {
+  try {
+    const dados = data.body;
+    const { nome, cpf } = dados;
+    await editDatabase(nome, cpf);
+    req.send({ resposta: "OK", status: 200 });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
 
 
 async function databaseConnect() {
@@ -34,7 +48,6 @@ async function databaseConnect() {
     const connectDataBase = await client.connect();
     const databaseDocument = await connectDataBase.db(dataBaseName);
     const collectioon = await databaseDocument.collection(dataBaseName);
-    console.log(collectioon);
     return collectioon;
   } catch (error) {
     console.log(error);
@@ -47,20 +60,40 @@ const consultarDatabase = async () => {
     const databaseConsultFromMongo = await data.find().toArray();
     return databaseConsultFromMongo;
   } catch (error) {
-    console.log('error database consult');
+    console.log("error database consult");
   }
 };
 
-
-const databaseCreate = async(cpf, name) => {
+const databaseCreate = async (name, cpf) => {
   try {
     const data = await databaseConnect();
-    const dataBaseNewDados = await data.insertOne({cpfPerson: cpf, namePreson: name})
-    console.log('sucess database dados new');
+    const dataBaseNewDados = await data.insertOne({
+      namePerson: name,
+      cpfPerson: cpf,
+    });
+    console.log(dataBaseNewDados);
+    console.log("sucess database dados new");
   } catch (error) {
     console.log(error);
   }
+};
+
+async function editDatabase(name, cpf) {
+  try {
+    const data = await databaseConnect();
+    const httpUpdateDataBase = await data.updateOne(
+      { namePerson: name },
+      { $set: { cpfPerson: cpf } }
+    );
+    console.log(httpUpdateDataBase);
+    console.log("sucess database Update from mongodb");
+  } catch (error) {
+    console.log("error database from update mongodb");
+  }
 }
+
+
+
 
 api.listen(8080, () => {
   console.log("servidor rodando na porta 8080");
